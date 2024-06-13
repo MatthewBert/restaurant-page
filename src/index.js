eb67fdc63b0464178c8c810410b42bdebf8849e7
MatthewBert <47572136+MatthewBert@users.noreply.github.com>
2024-06-13 13:26:46 -0400

    updated and changed

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("Hello World!");
+import './style.css'
+import createHomePage from './home';
+
+const content = document.getElementById('content');
+
+function loadHomePage(){
+    content.innerHTML = '';
+    content.appendChild(createHomePage());
+}
+
+document.getElementById('homebutton').addEventListener('click', loadHomePage);
+
+loadHomePage(); //load the home page by default
