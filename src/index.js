9d27e0e6aa8d67389816fd684723d0ba34eb0c6d
MatthewBert <47572136+MatthewBert@users.noreply.github.com>
2024-06-13 14:52:31 -0400

    added functionality for about button

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import './style.css'
 import createHomePage from './home';
 import createMenuPage from './menu';
+import createAboutPage from './about';
 
 const content = document.getElementById('content');
 
-function loadHomePage(){
+function loadHomePage() {
     content.innerHTML = '';
     content.appendChild(createHomePage());
 }
 
-function loadMenuPage(){
+function loadMenuPage() {
     content.innerHTML = '';
     content.appendChild(createMenuPage());
 }
 
+function loadAboutPage() {
+    content.innerHTML = '';
+    content.appendChild(createAboutPage());
+}
+
 document.getElementById('homebutton').addEventListener('click', loadHomePage);
 document.getElementById('menubutton').addEventListener('click', loadMenuPage);
+document.getElementById('aboutbutton').addEventListener('click', loadAboutPage);
 
 loadHomePage(); //load the home page by default
