8804a5536176493dc96fd1a71dfbef19418acc0b
MatthewBert <47572136+MatthewBert@users.noreply.github.com>
2024-06-13 14:20:45 -0400

    functionality for menu button

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css'
 import createHomePage from './home';
+import createMenuPage from './menu';
 
 const content = document.getElementById('content');
 
@@ -8,6 +9,12 @@ function loadHomePage(){
     content.appendChild(createHomePage());
 }
 
+function loadMenuPage(){
+    content.innerHTML = '';
+    content.appendChild(createMenuPage());
+}
+
 document.getElementById('homebutton').addEventListener('click', loadHomePage);
+document.getElementById('menubutton').addEventListener('click', loadMenuPage);
 
 loadHomePage(); //load the home page by default
